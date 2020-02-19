97c5505dfb2c8e6b925c6586061fbe4e75d79f79
Gunnar Grosch <gunnar@grosch.se>
2020-02-19 20:15:49 +0100

    Added

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gunnar Starter Blog`,
+    title: `Gunnar's Starter Blog`,
     author: `Kyle Mathews`,
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
